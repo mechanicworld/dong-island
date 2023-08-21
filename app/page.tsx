a776cf0d000ceed64ce93d86016ffe649027cfef
mechanicworld <47930174+mechanicworld@users.noreply.github.com>
2023-08-21 19:27:50 +0300

    Initialize dong-island

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="">
+    <p >
+    Welcome to Dong-Island. This is the most nobel of all games. Sit back and enjoy the ride. Board will be design for your role playing game needs. Of course, you can use it for any game you want. But for more customizing you have to wait me to find time to do it as well.  
+    </p>
+
+    </main>
+  )
+}
---
+++ app/room/page.tsx
@@ -0,0 +1,8 @@
+export default function Rooms() {
+  return (
+    <main className="">
+      <h1>Create a room </h1>
+      <div></div>
+    </main>
+  );
+}
